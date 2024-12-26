c732eda8cba0237b492e49bb8b7c57db25f48303
vimal  kumar <vneeclasses@gmail.com>
2024-12-26 20:34:27 +0000

    Datatypes

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,25 @@
+// "use strict"  //treat all js code as newer version
+
+// alert(3+3)  //we are using nodejs not browser 
+
+//code readability that should be high
+
+
+
+// types of datatype 
+
+// 1. primitive datatype
+
+//number => 2 to power 53
+//bigInt => mostly use in higher data value like trading 
+//string => "vimal"
+//boolean => true/false 
+//null => standalone value 
+//undefined => let number ;
+//symbol => UNIQUE , mostly  use in react 
+
+// Object 
+
+console.log(typeof null) //Object
+
+console.log(typeof undefined)  //undefines
