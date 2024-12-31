63691295ddb632d3747e4e8c4e0309094073bb4d
vimal  kumar <vneeclasses@gmail.com>
2024-12-31 07:33:36 +0000

    4, 5th class

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -39,8 +39,42 @@ let BooleanIsLoggedIn = Boolean(isLoggesIn)
 
 let  someNumber = 33
 
-console.log(String(someNumber))
-console.log(typeof String(someNumber))   //it changed in string 
+// console.log(String(someNumber))
+// console.log(typeof String(someNumber))   //it changed in string 
+
+
+
+// **********************************OPERATIONS*******************************************//
+
+
+let value =  3
+
+let negValue = -value
+//console.log(negValue)
+
+let str1 = "hello"
+let str2 = " vimal"
+let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log("1" + 2) //12
+// console.log(1 + "2")  //12
+// console.log(1 + 3 + "3") //43
+// console.log("3" + 1 +2) //312
+
+// console.log(true) //true 
+// console.log(+true) //1
+// console.log(+"") //0
+
+// prefic and postfix => increment and decrement  =>  homework reading at mdn
+
+
+
+
+
+
+
 
 
 
---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,6 @@
+// console.log(2>1);
+
+
+console.log(null > 0)  //false
+console.log(null == 0) //false
+console.log(null >= 0)  //true  // jab ham comparision karte hai to null ko as a 0 use kiya jata hai 
